3fee1c52cf185cf39f96374a3b3a2de9d36e0fdf
lcuvelli <lcuvelli@ulb.be>
2021-04-11 00:56:16 +0200

    Barre de navigation pour airflow.html

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
-from flask import request, escape, flash, Markup
+from flask import request, flash, Markup
 import air_flow_rate as airflow
 import sys
 
-
-
 app = Flask(__name__)
 app.secret_key = "key"
+#Bootstrap(app)
 
+#nav.init_app(app)
+
+
+nav = Markup('<nav class="nav justify-content-center" style="background-color: rgb(19,74,145) ">'
+             + '<a class="nav-link active" href="#">Accueil</a>'
+             + '<a class="nav-link" href="./">Home</a> '
+             + '<a class="nav-link" href="about">About</a>'
+             + '<a class="nav-link" href="contacts">Contacts</a>'
+             + ' </nav>')
+print(nav)
 
 @app.route("/")
 def index():
@@ -44,21 +53,10 @@ def index():
                 flash(message)
 
             print(Q)
+    context = {"active": "Miniaml Air Flow"}
 
-    return render_template('index.html', Q=Q, mass_to_evaporate = mass_to_evaporate)
+    return render_template('index.html', nav=nav, Q=Q, mass_to_evaporate = mass_to_evaporate, context=context)
 
-
-
-
-
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
 
 
 @app.route("/contacts")
@@ -73,6 +71,46 @@ def about():
 @app.route("/t")
 def test():
     return render_template('test.html')
+
+@app.route("/airflow")
+def airflow():
+    Q = ""
+    mass_to_evaporate = ""
+    RHamb = request.args.get("RHamb", type=float)
+    Tamb = request.args.get("Tamb", type=float)
+    M0 = request.args.get("M0", type=float)
+    X0 = request.args.get("X0", type=float)
+    Xf = request.args.get("Xf", type=float)
+    td = request.args.get("td", type=float)
+    Td = request.args.get("Td", type=float)
+
+    i = 0
+
+    if RHamb and Tamb and M0 and X0 and Xf and td and Td:
+        if Xf >= X0   :
+            message = Markup('Warning: Final moisture content of the product (Xf) should be <b>lower</b> than inital moisture (X0).')
+            i += 1
+            flash(message)
+
+        if Tamb >= Td :
+            message = Markup('Warning: Ambient temperature (Tamb) should be <b>lower</b> than drying temperature (Td).')
+            flash(message)
+            i += 1
+
+        elif (i==0):
+            mass_to_evaporate = M0 / (1 + X0) * (X0 - Xf)
+            Q = airflow.compute_air_flow_rate(RHamb, Tamb, M0, X0, Xf, td, Td)
+            if Q < 0 :
+                message = Markup(
+                    'Error: Minimal air flow is negative. Not feasible with conditions given. Please try again.')
+                flash(message)
+            print(Q)
+
+    context = {"active": "Miniaml Air Flow"}
+
+    return render_template('airflow.html', Q=Q, mass_to_evaporate = mass_to_evaporate, context=context)
+
+
 
 
 if __name__ == "__main__":
